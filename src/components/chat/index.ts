9f29c25996a10a1defe236d3fe0c943e8444e6c9
lex <rika00126@icloud.com>
2026-04-01 23:22:58 +0800

    feat: 集成 AI 聊天浮动按钮到全局布局

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -1,6 +1,5 @@
 export { ChatMessages, type Message } from './ChatMessages'
 export { ChatInput } from './ChatInput'
 export { ConversationList } from './ConversationList'
-// These will be exported by later tasks:
-// export { ChatFloatingButton } from './ChatFloatingButton'
-// export { ChatDialog } from './ChatDialog'
+export { ChatFloatingButton } from './ChatFloatingButton'
+export { ChatDialog } from './ChatDialog'
